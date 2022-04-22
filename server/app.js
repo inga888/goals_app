5c4ab0a0e666319561eb46e27afc5f8d3313062e
Inga Zhamsoeva <injoyme@gmail.com>
2022-04-22 20:58:03 +0300

    goalscontroller and routes setup

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
-console.log('hello');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//миддлвары
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use(errorHandler); //перекроет дефолтную обраюотку ошибок экспресс
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
